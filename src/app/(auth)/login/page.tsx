0753f093e897a48893455a3a860b872291648867
amora7741 <mora7741@gmail.com>
2024-07-10 12:15:31 -0700

    Add loader to sign in butotn

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,6 +6,7 @@ import { signIn } from 'next-auth/react';
 import { WavyBackground } from '@/components/ui/background-waves';
 import { FcGoogle } from 'react-icons/fc';
 import { BsChatLeftHeartFill } from 'react-icons/bs';
+import { Loader2 } from 'lucide-react';
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
@@ -25,19 +26,21 @@ const Login = () => {
 
   return (
     <WavyBackground backgroundFill='white'>
-      <div className='bg-blue-300/70 flex flex-col rounded-xl w-screen h-screen text-center md:w-fit md:h-fit items-center justify-center gap-8 md:justify-around md:p-8 md:px-24'>
-        <BsChatLeftHeartFill
-          fill='white'
-          className='absolute top-12 md:static size-20 md:size-10'
-        />
-        <h1 className='font-bold text-6xl md:text-5xl text-white'>Sign In</h1>
+      <div className='bg-blue-300/70 flex flex-col rounded-xl w-screen h-screen text-center md:w-fit md:h-fit items-center justify-center gap-8 md:p-8 md:px-24'>
+        <BsChatLeftHeartFill fill='white' className='size-12 md:size-10' />
+        <h1 className='font-bold text-5xl md:text-4xl text-white'>Sign In</h1>
         <Button
-          className='bg-white/10 hover:bg-white/30 gap-4 text-lg'
+          className='bg-white/10 hover:bg-white/30 gap-4 text-lg relative'
           onClick={loginWithGoogle}
           disabled={loading}
         >
-          <FcGoogle size={20} />
-          Sign in with Google
+          {loading && <Loader2 className='size-8 absolute animate-spin' />}
+          <div
+            className={`${loading ? 'invisible' : ''} flex gap-2 items-center`}
+          >
+            <FcGoogle size={20} />
+            Sign in with Google
+          </div>
         </Button>
       </div>
     </WavyBackground>
