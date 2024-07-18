1c56b375d6584d9981b36aa559840359ecb247db
amora7741 <mora7741@gmail.com>
2024-07-18 15:59:14 -0700

    Implement add friend api route

--- src/components/IncomingFriendRequests.tsx
+++ src/components/IncomingFriendRequests.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { UserRoundPlus } from 'lucide-react';
 import LoadingButton from './LoadingButton';
 import { X, Check } from 'lucide-react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from '@/components/ui/use-toast';
 
 const IncomingFriendRequests = ({
   incomingFriendRequests,
@@ -18,6 +21,62 @@ const IncomingFriendRequests = ({
 
   const [denyLoading, setDenyLoading] = useState(false);
   const [acceptLoading, setAcceptLoading] = useState(false);
+
+  const router = useRouter();
+
+  const acceptRequest = async (senderID: string) => {
+    try {
+      setAcceptLoading(true);
+
+      await axios.post('/api/friends/accept', { id: senderID });
+
+      setFriendRequests((prev) =>
+        prev.filter((request) => request.senderID !== senderID)
+      );
+
+      router.refresh();
+
+      toast({
+        variant: 'success',
+        title: 'Friend request accepted!',
+      });
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Error',
+        description: 'An error occurred while accepting the request.',
+      });
+    } finally {
+      setAcceptLoading(false);
+    }
+  };
+
+  const denyRequest = async (senderID: string) => {
+    try {
+      setDenyLoading(true);
+
+      await axios.post('/api/friends/deny', { id: senderID });
+
+      setFriendRequests((prev) =>
+        prev.filter((request) => request.senderID !== senderID)
+      );
+
+      router.refresh();
+
+      toast({
+        variant: 'success',
+        title: 'Friend request successfully denied.',
+      });
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Error',
+        description: 'An error occurred while denying the request.',
+      });
+    } finally {
+      setDenyLoading(false);
+    }
+  };
 
   return (
     <>
@@ -35,12 +94,14 @@ const IncomingFriendRequests = ({
               <LoadingButton
                 className='bg-green-400 hover:bg-green-500'
                 loading={acceptLoading}
+                onClick={() => acceptRequest(request.senderID)}
               >
                 <Check />
               </LoadingButton>
               <LoadingButton
                 className='bg-red-500 hover:bg-red-600'
                 loading={denyLoading}
+                onClick={() => denyRequest(request.senderID)}
               >
                 <X />
               </LoadingButton>
