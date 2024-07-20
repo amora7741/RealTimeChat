9b69f156e7b2bcb8d265a6c9d8c8b9d1bd13fa7a
amora7741 <mora7741@gmail.com>
2024-07-19 21:16:34 -0700

    Create loading pages for requests and chat pages

---
+++ src/app/(home)/home/requests/loading.tsx
@@ -0,0 +1,18 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+const LoadingPage = () => {
+  return (
+    <div className='flex flex-col p-6 w-full'>
+      <Skeleton className='w-[460px] h-[40px] mb-8' />
+      <Skeleton className='w-[250px] h-[20px] mb-8' />
+
+      <div className='flex flex-col gap-y-4'>
+        <Skeleton className='w-full h-[40px]' />
+        <Skeleton className='w-full h-[40px]' />
+        <Skeleton className='w-full h-[40px]' />
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
